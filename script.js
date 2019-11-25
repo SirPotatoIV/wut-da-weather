1626bd8208303c0934e874ffd24fa02fbdb0fd9c
SirPotatoIV <otoolej33@gmail.com>
2019-11-24 19:04:26 -0600

    Now request for one day of weather and the uv index for that day are working correctly.

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function weatherForecast() {
                 
                 // let userCity = cityInputEl.value;
                 let userCity = "minneapolis"; 
-                console.log('You searched for this city: ',userCity);
+                // console.log('You searched for this city: ',userCity);
                 searchForCityWeather(userCity);
             });
             
@@ -29,15 +29,12 @@ function weatherForecast() {
         
     // Function will be used to perform request to the weather API
     function searchForCityWeather(userCity){
-        // Currently AyDy's key for the weather API.
+        // Jake's apiKey
         const apiKey = "14148f20140310fac55bc379dbdb7119";
-        let lat = "";
-        let lon = "";
         // let cityName = userCity;
         let cityName = userCity;
         const oneDayWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&APPID="+apiKey;
-        const uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?appid="+apiKey+"&lat="+lat+"&lon="+lon;
-        const oneDayWeather = {
+        let oneDayWeather = {
             temp: "",
             humidity: "",
             windSpeed: "",
@@ -45,21 +42,35 @@ function weatherForecast() {
             lon: "",
             uvIndex: ""
         };
-        
+
         // Performs request to the weather API with get
         axios.get(oneDayWeatherURL) 
         .then(function(response) {
-            
             
             oneDayWeather.temp = response.data.main.temp;
             oneDayWeather.humidity = response.data.main.humidity;
             oneDayWeather.windSpeed = response.data.wind.speed;
             oneDayWeather.lat = response.data.coord["lat"];
             oneDayWeather.lon = response.data.coord["lon"];
+            // Takes values recieved from this get and passess them on to requestUVIndex. 
+            // Tried setting oneDayWeather.uvIndex in this function, but it would set the value before the function call completed.
+            requestUVIndex(oneDayWeather, apiKey)
+        });
+       
+    }
+    
+    function requestUVIndex(oneDayWeather, apiKey){
+        
+        const uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?appid="+apiKey+"&lat="+oneDayWeather.lat+"&lon="+oneDayWeather.lon;
+        
+        axios.get(uvIndexURL) 
+        .then(function(response) {
+            
+            oneDayWeather.uvIndex = response.data.value;
             console.log(oneDayWeather);
+        
         });
         
-
     }
             
 }
